b89e32d5ae891b92f4c58c9c60a740a29c40c4a5
Luuk Holleman <luukholleman@gmail.com>
2014-05-27 16:49:11 +0200

    Moved partials to partial view

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,7 +1,8 @@
 <!doctype html>
 <html>
     <head>
-	    <meta name="viewport" content="width=device-width">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	    <link rel="stylesheet" type="text/css" title="dark" href="/css/lib/bootstrap/bootstrap-dark.css">
 	    <link rel="stylesheet" type="text/css" title="dark" href="/css/lib/bootstrap/bootstrap-dark.css">
 	    <link rel="alternate stylesheet" type="text/css" title="light" href="/css/lib/bootstrap/bootstrap-light.css">
 		{{ HTML::style('css/bootstrap-extend.css') }}
@@ -9,9 +10,9 @@
 		{{ HTML::style('css/colors.css') }}
 		{{ HTML::style('css/lib/angular/loader-bar.css') }}
     </head>
-    <body ng-app="App">
+    <body>
 	    <div class="container">
-		    @include('partials.nav')
+		    @include('partial.nav')
 
 		    <div class="page-header">
 			    @yield('header')
@@ -19,18 +20,6 @@
 
 		    @yield('content')
 	    </div>
-
-<!--	    {{ HTML::script('js/lib/jquery/jquery-2.1.0.js') }}-->
-<!--	    {{ HTML::script('js/lib/underscore/underscore-1.6.0.js') }}-->
-<!--		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.js"></script>-->
-<!--		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-route.min.js"></script>-->
-<!--		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular-animate.js"></script>-->
-<!--	    {{ HTML::script('js/lib/angular/ui-bootstrap.js') }}-->
-<!--	    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/restangular/1.3.1/restangular.js"></script>-->
-<!--	    {{ HTML::script('js/lib/angular/loader-bar.js') }}-->
-<!--        {{ HTML::script('js/app.js') }}-->
-<!--        {{-- HTML::script('js/angular/hero.js') --}}-->
-<!--        {{-- HTML::script('js/style_switcher.js') --}}-->
 
         {{ HTML::script('js/debug.js') }}
         {{-- HTML::script('http://cssrefresh.frebsite.nl/js/cssrefresh.js') --}}
