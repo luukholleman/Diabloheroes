bbbbb4edc7f8bb142ba497eae1a99bceb4120eaf
Luuk Holleman <luukholleman@gmail.com>
2014-05-30 21:50:39 +0200

    New home view

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -10,67 +10,49 @@
 	</h1>
 @stop
 
+@section('sidebar')
+<ul class="nav nav-pills nav-stacked">
+	<li class="disabled">
+		<a href="#" class="ranklist-filter">
+			Playstyle
+		</a>
+	</li>
+	<li class="{{ active_class($mode->isSoftcore()) }}">
+		<a href="{{ URL::route('home.ranklist', [$currentRanklist->stat, 'softcore']) }}">
+			Softcore
+		</a>
+	</li>
+	<li class="{{ active_class($mode->isHardcore()) }}">
+		<a href="{{ URL::route('home.ranklist', [$currentRanklist->stat, 'hardcore']) }}">
+			Hardcore
+		</a>
+	</li>
+	<li class="{{ active_class($mode->isBoth()) }}">
+		<a href="{{ URL::route('home.ranklist', [$currentRanklist->stat, 'both']) }}">
+			Both
+		</a>
+	</li>
+	@foreach($ranklistCategories as $ranklistCategory)
+		<li class="disabled">
+			<a href="#" class="ranklist-filter">
+				{{ $ranklistCategory->name }}
+			</a>
+		</li>
+		@foreach($ranklistCategory->ranklists as $ranklist)
+			<li class="{{ active_class($currentRanklist->id == $ranklist->id) }}">
+				<a href="{{ URL::route('home.ranklist', [$ranklist->stat, $mode]) }}">
+					{{ $ranklist->name }}
+				</a>
+			</li>
+		@endforeach
+	@endforeach
+</ul>
+@stop
+
 @section('content')
-	<div class="row">
-		<div class="col-xs-12 col-sm-2">
-			<h2>Filters</h2>
-			<ul class="nav nav-pills nav-stacked">
-                <li class="disabled">
-                    <a href="#" class="ranklist-filter">
-                        Playstyle <i class="fa fa-plus-square-o"></i>
-                    </a>
-	                <ul class="nav nav-pills nav-stacked {{ (Agent::isMobile()) ? 'hidden' : '' }}">
-		                <li class="{{ active_class($mode == 'softcore') }}">
-			                <a href="{{ URL::route('home.ranklist', [$currentRanklist->stat, 'softcore']) }}">
-				                Softcore
-			                </a>
-		                </li>
-		                <li class="{{ active_class($mode == 'hardcore') }}">
-			                <a href="{{ URL::route('home.ranklist', [$currentRanklist->stat, 'hardcore']) }}">
-				                Hardcore
-			                </a>
-		                </li>
-		                <li class="{{ active_class($mode == 'both') }}">
-			                <a href="{{ URL::route('home.ranklist', [$currentRanklist->stat, 'both']) }}">
-				                Both
-			                </a>
-		                </li>
-	                </ul>
-                </li>
-				@foreach($ranklistCategories as $ranklistCategory)
-					<li class="disabled">
-						<a href="#" class="ranklist-filter">
-							{{ $ranklistCategory->name }} <i class="fa fa-plus-square-o"></i>
-						</a>
-						<ul class="nav nav-pills nav-stacked {{ (Agent::isMobile()) ? 'hidden' : '' }}"">
-							@foreach($ranklistCategory->ranklists as $ranklist)
-								<li class="{{ active_class($currentRanklist->id == $ranklist->id) }}">
-									<a href="{{ URL::route('home.ranklist', [$ranklist->stat, $mode]) }}">
-										{{ $ranklist->name }}
-									</a>
-								</li>
-							@endforeach
-						</ul>
-					</li>
-				@endforeach
-			</ul>
-		</div>
-		<div class="col-xs-12 col-sm-10">
-			@if(Agent::isMobile())
-				<a name="ranks"></a>
-				{{ $ranks->fragment('ranks')->links('pagination::simple') }}
-			@else
-				{{ $ranks->links('pagination::simple') }}
-			@endif
+			{{ $ranks->links('pagination::simple') }}
 
 			@yield('ranks')
 
-			@if(Agent::isMobile())
-				<a name="ranks"></a>
-				{{ $ranks->fragment('ranks')->links('pagination::simple') }}
-			@else
-				{{ $ranks->links('pagination::simple') }}
-			@endif
-		</div>
-	</div>
+			{{ $ranks->links('pagination::simple') }}
 @stop
