2ec130b10117866d14f4a1ab917b266fec886c67
Luuk Holleman <luukholleman@gmail.com>
2014-06-14 12:41:02 +0200

    Forgot namespaces

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -6,7 +6,7 @@ use RankRepository;
 /**
  * Class HomeController
  */
-class HomeController extends BaseController {
+class HomeController extends \BaseController {
 
 	/**
 	 * @var RankRepository
@@ -38,19 +38,19 @@ class HomeController extends BaseController {
 	{
 		$ranklist = $this->ranklistRepository->getRanklistByStat($ranklistStat);
 
-		if($ranklist->ranklistCategory->type == Ranklist::HERO)
-			return $this->showHeroRanklist($ranklist, new Mode($mode), $region);
+		if($ranklist->ranklistCategory->type == \Ranklist::HERO)
+			return $this->showHeroRanklist($ranklist, new \Mode($mode), $region);
 		else
-			return $this->showCareerRanklist($ranklist, new Mode($mode), $region);
+			return $this->showCareerRanklist($ranklist, new \Mode($mode), $region);
 	}
 
-	private function showHeroRanklist(Ranklist $ranklist, Mode $mode, $region)
+	private function showHeroRanklist(\Ranklist $ranklist, \Mode $mode, $region)
 	{
 		$ranklistCategories = $this->ranklistRepository->getAllCategories();
 
 		$ranks = $this->rankRepository->getTop($mode->bool(), $ranklist)->paginate(20);
 
-		return View::make('home.hero')
+		return \View::make('home.hero')
 			->with('currentRanklist', $ranklist)
 			->with('ranklistCategories', $ranklistCategories)
 			->with('ranks', $ranks)
@@ -58,13 +58,13 @@ class HomeController extends BaseController {
 			->with('rankMultiplier', 1 + 20 * ($ranks->getCurrentPage() - 1));
 	}
 
-	private function showCareerRanklist(Ranklist $ranklist, Mode $mode, $region)
+	private function showCareerRanklist(\Ranklist $ranklist, \Mode $mode, $region)
 	{
 		$ranklistCategories = $this->ranklistRepository->getAllCategories();
 
 		$ranks = $this->rankRepository->getTop($mode->bool(), $ranklist)->paginate(20);
 
-		return View::make('home.career')
+		return \View::make('home.career')
 			->with('currentRanklist', $ranklist)
 			->with('ranklistCategories', $ranklistCategories)
 			->with('ranks', $ranks)
